7c50cae3fa981fef2c2ad4e9dd5f8e143e9720f0
Kovalenko Evgeny <gransmastas@gmail.com>
2024-07-02 15:43:30 +0300

    analysis of the method

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-const num = a 
-let index = [5,5,3,2,5,6,7,8,6,5,8,9,10]
+const arr = new Array();
+
+
+// arr.reduce(cb, init)
+
+
+const multipleFn = (acc, item, index, array) => {
+  const newValue = item * 2;
+  const result = [...acc, newValue];
+  return result;
+}
+
+
+// arr.reduce(cb, init)
+
+
+const reduceFn = (arr, cb, init) => {
+  let accum = init;
+  for (let i = 0; i < arr.length; i++) {
+    accum = cb(accum, arr[i], i, arr)
+  }
+  return accum;
+}
+
+
+const example = [3, 5, 7];
+
+
+console.log('>>>>', reduceFn(example, multipleFn, []))
+
+
+console.log('>>>>>>>>', example.reduce((acc, el) => {
+  const newValue = el * 2;
+  acc.push(newValue);
+  return acc;
+}, []))
